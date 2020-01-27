1d7bccaa77d09d82678eb3c9443e099b524be619
tel3port <carn3lian@gmail.com>
2020-01-27 08:54:12 +0300

    tagged bot 2 not really done

--- shameless_hack.py
+++ shameless_hack.py
@@ -3,5 +3,5 @@ import os
 while True:
     os.system("python TaggedBot.py")
     os.system("heroku dyno:restart --app tagged-bot-2-usa")
-    os.system("heroku run 'heroku auth:token'")
+    os.system("heroku run 'heroku auth:token")
 
