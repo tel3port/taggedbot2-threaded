2d0b8cf44bff3f48a8f03dd89370e6f52f6f45ee
tel3port <carn3lian@gmail.com>
2020-01-27 06:06:21 +0300

    tagged bot 2 not really done

--- shameless_hack.py
+++ shameless_hack.py
@@ -3,5 +3,3 @@ import os
 while True:
     os.system("python TaggedBot.py")
     os.system("heroku restart --app tagged-bot-2-usa")
-    os.system("login 2ksaber@gmail.com")
-    os.system("password 4707b798-2d3a-4cc2-a03c-e878048cf24a")
